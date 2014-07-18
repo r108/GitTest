22479ca80ef6188a953a86192fcc2110c74400a1
r108 <katonaroland@msn.com>
2014-07-18 15:19:56 +0100

    first time commit

---
+++ GitTest/src/egit/PrimeNumber.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package egit;
+
+/**
+ * @author TIMI
+ *
+ */
+public class PrimeNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git");
+	}
+
+}
